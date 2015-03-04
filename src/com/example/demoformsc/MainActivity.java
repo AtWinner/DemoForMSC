3e07831c44f5bd76f806539e7d4f0525332be287
huhu <hufanglin@gmail.com>
2015-03-04 13:31:10 +0800

    大致完成一个数据表的设计
    2015-3-4 13:31:08

--- src/com/example/demoformsc/MainActivity.java
+++ src/com/example/demoformsc/MainActivity.java
@@ -1,27 +1,30 @@
 package com.example.demoformsc;
 
-import com.example.adapter.ListViewAdapter;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
-import com.handmark.pulltorefresh.library.PullToRefreshGridView;
-import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.os.Build;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-	private PullToRefreshListView mPullRefreshListView;
-	private ListView mListView;
-	
+	private PullToRefreshScrollView mPullToRefreshScrollView;
+	private ScrollView mScrollView;
+	private LinearLayout SrcollLinear;
+	private HorizontalScrollView horizontalScrollView;
+	private LinearLayout horizontalLinear;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,19 +35,40 @@ public class MainActivity extends ActionBarActivity {
 	}
 	private void init()
 	{
-		mPullRefreshListView = (PullToRefreshListView)findViewById(R.id.pull_refresh_list);
-		mListView = mPullRefreshListView.getRefreshableView();
-		
-		ListViewAdapter adapter = new ListViewAdapter(MainActivity.this, R.layout.list_item);
-		 mListView.setAdapter(adapter);
+		mPullToRefreshScrollView = (PullToRefreshScrollView)findViewById(R.id.pull_refresh_list);
+		mPullToRefreshScrollView.setMode(Mode.BOTH);
+		mScrollView = mPullToRefreshScrollView.getRefreshableView();
+		horizontalScrollView = (HorizontalScrollView)findViewById(R.id.horizontalScrollView);
+		horizontalScrollView.smoothScrollTo(100, 0);
+		horizontalLinear = (LinearLayout)findViewById(R.id.horizontalLinear);
+//		horizontalLinear.scrollTo(1000, 0);
+		new Handler().postDelayed((
+				new Runnable() 
+				{ 
+					@Override 
+					public void run() 
+					{ 
+						horizontalScrollView.scrollTo(horizontalScrollView.getLeft()-100,0); 
+						} 
+					}),5);
 	}
 	private void setParams()
 	{
-		mPullRefreshListView.setMode(Mode.BOTH);
+		
 	}
 	private void bindEvent()
 	{
-		
+		mPullToRefreshScrollView.setOnRefreshListener(new com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener2<ScrollView>() {
+			@Override
+			public void onPullDownToRefresh(PullToRefreshBase<ScrollView> refreshView) 
+			{
+				Toast.makeText(MainActivity.this, "text", Toast.LENGTH_SHORT).show();
+			}
+			@Override
+			public void onPullUpToRefresh(PullToRefreshBase<ScrollView> refreshView) 
+			{
+			}
+		});
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
